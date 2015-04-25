256e03d8eadf311ed06db07638a7f9485e1d2081
Jason Raimondi <jason@raimondi.us>
2015-04-25 11:24:51 -0400

    initial setup with routeparams working

--- resources/assets/js/app/controllers.js
+++ resources/assets/js/app/controllers.js
@@ -5,11 +5,48 @@ myApp.config(['$routeProvider',
     $routeProvider.
       when('/', {
         templateUrl: 'pages/home.html',
+        controller: 'HomeController'
       }).
-      when('/sink', {
-        templateUrl: 'pages/kitchen-sink.html'
+      when('/projects', {
+        templateUrl: 'pages/projects.html',
+        controller: 'ProjectController'
+      }).
+      when('/projects/:slug', {
+        templateUrl: 'pages/project.html',
+        controller: 'ProjectController'
+      }).
+      when('/about', {
+        templateUrl: 'pages/about.html'
+      }).
+      when('/contact', {
+        templateUrl: 'pages/contact.html'
       }).
       otherwise({
         redirectTo: '/'
       });
 }]);
+
+
+myApp.controller('HomeController', [ '$scope', '$http',
+
+  function ($scope, $http) {
+    $http.get('/data/skills.json').success(function(skills) {
+      $scope.skills = skills;
+    });
+    $http.get('/data/resume.json').success(function(resumes) {
+      $scope.resumes = resumes;
+    });
+    $http.get('/data/projects.json').success(function(projects) {
+      $scope.projects = projects;
+    });
+  }
+
+]);
+
+myApp.controller('ProjectController', [ '$scope', '$routeParams',
+
+  function ($scope, $routeParams, slug) {
+    $scope.params = $routeParams;
+  }
+
+]);
